f4e008db6dfa915b7e872e7f578bcab1a3daf78f
Paul Janson <pauljanson002@gmail.com>
2021-07-27 17:53:31 +0530

    Created development branch

--- pages/home.js
+++ pages/home.js
@@ -4,7 +4,7 @@ const Home = ()=>{
     return(
         <React.Fragment>
             <div>
-                <h1>Hello World</h1>
+                <h1>Hello World !!</h1>
             </div>
         </React.Fragment>
     )
