60f96c14a714337e397b8be7ce072567525fcf68
Paul Janson <pauljanson002@gmail.com>
2021-08-12 13:30:53 +0530

    Created development branch

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./Navigation";
+const Header = ()=>{
+  return(
+    <header>
+      <div className={"header"}>
+        <Navigation/>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Home from './home';
 
+
 const Pages = () => {
     return (
         <Router>
-                <Route exact path="/" component={Home} />
+              <Route exact path="/" component={Home} />
         </Router>
     );
 };
