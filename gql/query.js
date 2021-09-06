3ee081af47841adab775c2fd2314d1cb167f121d
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2021-09-07 01:50:26 +0530

    view service providers by type - need to refactor

--- gql/query.js
+++ gql/query.js
@@ -1,70 +1,119 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 const IS_LOGGED_IN = gql`
-    {
-        isLoggedIn @client
-    }
+  {
+    isLoggedIn @client
+  }
 `;
 
 const GET_ME = gql`
-    query Query {
-        me {
-            id
-            username
-            email
-            roles
-        }
+  query Query {
+    me {
+      id
+      username
+      email
+      roles
     }
+  }
 `;
 
-const GET_ME_AS_SERVICE_PROVIDER  = gql`
-    query Query {
-        me {
-            username
-            email
-            nic
-            profession
-            province
-            city
-            town
-            bio
-            service_providing_status
-            roles
-        }
+const GET_ME_AS_SERVICE_PROVIDER = gql`
+  query Query {
+    me {
+      username
+      email
+      nic
+      profession
+      province
+      city
+      town
+      bio
+      service_providing_status
+      roles
     }
-`
-const GET_ME_AS_SERVICE_REQUESTER  = gql`
-    query Query {
-        me {
-            username
-            email
-            city
-            address
-
-        }
+  }
+`;
+const GET_ME_AS_SERVICE_REQUESTER = gql`
+  query Query {
+    me {
+      username
+      email
+      city
+      address
     }
-`
+  }
+`;
 const GET_NOTE_FEED = gql`
-    query Query($jobPostingFeedProvince: String!, $jobPostingFeedCity: String!, $jobPostingFeedTown: String!, $jobPostingFeedCategory: String!, $jobPostingFeedCursor: String) {
-        jobPostingFeed(province: $jobPostingFeedProvince, city: $jobPostingFeedCity, town: $jobPostingFeedTown, category: $jobPostingFeedCategory, cursor: $jobPostingFeedCursor) {
-            jobPostings {
-                id
-                postedBy {
-                    username
-                }
-                description
-                budgetRange {
-                    lowerLimit
-                    upperLimit
-                }
-                location{
-                    city
-                    town
-                }
-            }
-            cursor
-            hasNextPage
+  query Query(
+    $jobPostingFeedProvince: String!
+    $jobPostingFeedCity: String!
+    $jobPostingFeedTown: String!
+    $jobPostingFeedCategory: String!
+    $jobPostingFeedCursor: String
+  ) {
+    jobPostingFeed(
+      province: $jobPostingFeedProvince
+      city: $jobPostingFeedCity
+      town: $jobPostingFeedTown
+      category: $jobPostingFeedCategory
+      cursor: $jobPostingFeedCursor
+    ) {
+      jobPostings {
+        id
+        postedBy {
+          username
         }
+        description
+        budgetRange {
+          lowerLimit
+          upperLimit
+        }
+        location {
+          city
+          town
+        }
+      }
+      cursor
+      hasNextPage
     }
-`
-export {IS_LOGGED_IN,GET_ME,GET_ME_AS_SERVICE_PROVIDER ,GET_NOTE_FEED,GET_ME_AS_SERVICE_REQUESTER}
+  }
+`;
+
+const GET_SERVICE_PROVIDERS_BY_NAME = gql`
+  query SearchServiceProviderName($searchServiceProviderbyNameName: String!) {
+    searchServiceProviderbyName(name: $searchServiceProviderbyNameName) {
+      id
+      username
+      profession
+      city
+      province
+      service_providing_status
+    }
+  }
+`;
+
+const GET_SERVICE_PROVIDER_BY_PROFESSION = gql`
+  query SearchServiceProviderByProfession(
+    $searchServiceProviderbyProfessionProfession: String!
+  ) {
+    searchServiceProviderbyProfession(
+      profession: $searchServiceProviderbyProfessionProfession
+    ) {
+      id
+      username
+      profession
+      city
+      bio
+      service_providing_status
+    }
+  }
+`;
+export {
+  IS_LOGGED_IN,
+  GET_ME,
+  GET_ME_AS_SERVICE_PROVIDER,
+  GET_NOTE_FEED,
+  GET_ME_AS_SERVICE_REQUESTER,
+  GET_SERVICE_PROVIDERS_BY_NAME,
+  GET_SERVICE_PROVIDER_BY_PROFESSION
+};
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,64 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import LoggedInRoute from '../components/utils/LoggedInRoute'
+import LoggedInRoute from '../components/utils/LoggedInRoute';
 import Home from './home';
-import Login from './login'
-import SignUp from './signup'
-import ServiceProviderRegisterPage from "./service_provider_registration";
-import Loader from "../components/utils/Loader";
-import ServiceProviderRoute from "../components/utils/ServiceProviderRoute";
-import ServiceProviderProfilePage from "./service_provider_profile";
+import Login from './login';
+import SignUp from './signup';
+import ServiceProviderRegisterPage from './service_provider_registration';
+import Loader from '../components/utils/Loader';
+import ServiceProviderRoute from '../components/utils/ServiceProviderRoute';
+import ServiceProviderProfilePage from './service_provider_profile';
 
-import FindJobsPage from "./find_jobs";
+import FindJobsPage from './find_jobs';
 
 import AddDetailsPage from './service_requester_addDetails';
 
 import FindServicePage from './find_service';
 
+import SelectOptionPage from './service_option';
+
 const Pages = () => {
-    return (
-        <Router>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login}/>
-              <Route exact path="/signup" component={SignUp}/>
-              <Route exact path="/service_requester/createRequest" component={FindServicePage}/>
-              <LoggedInRoute exact path="/service_provider/register" component={ServiceProviderRegisterPage}/>
-              <ServiceProviderRoute exact path={"/service_provider"} component={ServiceProviderProfilePage}/>
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route
+        exact
+        path="/service_requester/createRequest/:provider_id"
+        component={FindServicePage}
+      />
+      <Route
+        exact
+        path="/service_requester/selectOption/:type"
+        component={SelectOptionPage}
+      />
+      <LoggedInRoute
+        exact
+        path="/service_provider/register"
+        component={ServiceProviderRegisterPage}
+      />
+      <ServiceProviderRoute
+        exact
+        path={'/service_provider'}
+        component={ServiceProviderProfilePage}
+      />
 
-              <ServiceProviderRoute exact path={"/service_provider/find_jobs"} component={FindJobsPage}/>
+      <ServiceProviderRoute
+        exact
+        path={'/service_provider/find_jobs'}
+        component={FindJobsPage}
+      />
 
-              <LoggedInRoute exact path="/service_requester/addDetails" component={AddDetailsPage}/>
+      <LoggedInRoute
+        exact
+        path="/service_requester/addDetails"
+        component={AddDetailsPage}
+      />
 
-              <Route exact path={"/test"} component={Loader}/>
-        </Router>
-    );
+      <Route exact path={'/test'} component={Loader} />
+    </Router>
+  );
 };
 
-export default Pages
+export default Pages;
