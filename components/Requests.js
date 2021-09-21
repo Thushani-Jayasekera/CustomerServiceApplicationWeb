8cbefd4b4a01d6928d6efbdcc228f8b71bf16a9b
Thushani-Jayasekera <thushanij.18@cse.mrt.ac.lk>
2021-09-21 22:16:30 +0530

    view service requ status improve-react pagination

---
+++ components/Requests.js
@@ -0,0 +1,226 @@
+import React, { useState } from 'react';
+
+import { motion } from 'framer-motion';
+const _ = require('underscore');
+
+import {
+  PrimaryButton as PrimaryButtonBase,
+  DisabledPrimaryButton as DisabledButtonBase
+} from '../components/misc/Buttons.js';
+import tw from 'twin.macro';
+import styled from 'styled-components';
+import { gql, useMutation, useQuery } from '@apollo/client';
+import Loader from '../components/utils/Loader';
+
+import { useToasts } from 'react-toast-notifications';
+import { css } from 'styled-components/macro'; //eslint-disable-line
+
+import { SectionHeading } from '../components/misc/Headings.js';
+import FeatherIcon from 'feather-icons-react';
+import { Columns, Message } from 'react-bulma-components';
+
+import { REJECT_SR } from '../gql/mutation';
+
+import { CANCEL_SR } from '../gql/mutation';
+import {
+  BrowserRouter as Router,
+  Link,
+} from 'react-router-dom';
+import SlideshowWithPagination from 'react-slideshow-with-pagination';
+
+const HeaderRow = tw.div`flex justify-between items-center flex-col xl:flex-row`;
+const TwoColumn = tw.div`flex flex-col sm:flex-row justify-between`;
+const Column = tw.div`sm:w-5/12 flex flex-col`;
+const Heading = tw(SectionHeading)``;
+
+const CardContainer = tw.div`mt-10 w-full sm:w-1/2 md:w-1/3 lg:w-full sm:pr-10 md:pr-6 lg:pr-12`;
+const Card = tw(
+  motion.a
+)`bg-gray-200 rounded-b block max-w-xs mx-auto sm:max-w-none sm:mx-0`;
+
+const CardButton = tw(PrimaryButtonBase)`text-sm rounded-full m-5 bg-green-500`;
+const CardButton2 = tw(PrimaryButtonBase)`text-sm rounded-full m-5 bg-blue-600`;
+const CardButtonUnAvailable = tw(
+  DisabledButtonBase
+)`text-sm rounded-full m-5 bg-red-500`;
+
+const CardReview = tw.div`font-medium text-xs text-gray-600`;
+
+const CardText = tw.div`p-4 text-gray-900`;
+const CardTitle = tw.h5`text-lg font-semibold group-hover:text-primary-500`;
+const CardContent = tw.p`mt-1 text-sm font-medium text-gray-600`;
+
+const Requests = ({ requests, loading, state, user }) => {
+  const { addToast } = useToasts();
+  //const[values,setValues]=useState();
+
+  const [cancelServiceRequest, { loading_cancel, error_cancel }] = useMutation(
+    CANCEL_SR,
+    {
+      onCompleted: data => {
+        addToast('Successfully canceled request', { appearance: 'success' });
+        history.push(`/profile`);
+      },
+      onError: error => {
+        addToast('Failed ', { appearance: 'error' });
+      }
+    }
+  );
+
+  const [rejectServiceRequest, { loading_reject, error_reject }] = useMutation(
+    REJECT_SR,
+    {
+      onCompleted: data => {
+        addToast('Successfully rejected request', { appearance: 'success' });
+        history.push(`/profile`);
+      },
+      onError: error => {
+        addToast('Failed ', { appearance: 'error' });
+      }
+    }
+  );
+
+  if (loading || loading_reject || loading_cancel) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      {requests.length !== 0 ? (
+        <SlideshowWithPagination
+          showNumbers={true}
+          showDots={true}
+          showArrows={true}
+          autoplay={false}
+          enableMouseEvents={false}
+        >
+          {requests.map((request, index) => (
+            <CardContainer key={index}>
+              <Card
+                className="group"
+                href={request.url}
+                initial="rest"
+                whileHover="hover"
+                animate="rest"
+              >
+                <TwoColumn>
+                  <Column>
+                    <CardText>
+                      <CardTitle>{request.task}</CardTitle>
+                      <br />
+                      <Columns>
+                        <FeatherIcon icon="calendar" />
+                        <CardTitle> Date - {request.date}</CardTitle>
+                      </Columns>
+                      <br />
+                      <Columns>
+                        <FeatherIcon icon="clock" />
+                        <CardTitle>Time - {request.time} H</CardTitle>
+                      </Columns>
+                    </CardText>
+                  </Column>
+
+                  <Column>
+                    <Columns>
+                      <Link
+                        to={{
+                          pathname: `/service_request/${request.id}`
+                        }}
+                      >
+                        <CardButton2 className="button is-info  w-max px-6 py-3 font-bold text-gray-100 hocus:bg-primary-700 hocus:text-gray-200 focus:shadow-outline focus:outline-none transition duration-300 text-sm rounded-full m-5">
+                          View
+                        </CardButton2>
+                      </Link>
+                     
+                    </Columns>
+
+                    {user === 'Provider' ? (
+                      <>
+                        <Link
+                        to={{
+                          pathname: `/service_request/${request.id}`
+                        }}
+                      >
+                        <CardButton2
+                          disabled={state !== 'Pending' || user != 'Provider'}
+                          hidden={user != 'Provider'}
+                          className="button is-success  px-6 py-3 font-bold text-gray-100 hocus:bg-primary-700 hocus:text-gray-200 focus:shadow-outline focus:outline-none transition duration-300 text-sm rounded-full m-5"
+                        >
+                          View and Accept
+                        </CardButton2>
+                        </Link>
+
+                        <CardButton2
+                          disabled={
+                            (state !== 'Pending' && state !== 'Accepted') ||
+                            user != 'Provider'
+                          }
+                          hidden={user != 'Provider'}
+                          className="button is-danger"
+                          onClick={event => {
+                            rejectServiceRequest({
+                              variables: {
+                                rejectServiceRequestId: request.id
+                              }
+                            });
+                          }}
+                        >
+                          Reject
+                        </CardButton2>
+
+                        <CardButton2
+                          disabled={state !== 'Started' || user != 'Provider'}
+                          hidden={user != 'Provider'}
+                          className="button is-success  px-6 py-3 font-bold text-gray-100 hocus:bg-primary-700 hocus:text-gray-200 focus:shadow-outline focus:outline-none transition duration-300 text-sm rounded-full m-5"
+                        >
+                          Mark Completed
+                        </CardButton2>
+
+
+                      </>
+                    ) : (
+                      <>
+                        <CardButton2
+                          disabled={
+                            (state !== 'Pending' && state !== 'Accepted') ||
+                            user != 'Requester'
+                          }
+                          hidden={user != 'Provider'}
+                          className="button is-danger"
+                          onClick={event => {
+                            cancelServiceRequest({
+                              variables: {
+                                cancelServiceRequestId: request.id
+                              }
+                            });
+                          }}
+                        >
+                          Cancel
+                        </CardButton2>
+
+
+                        <CardButton2
+                          disabled={state !== 'Started' || user != 'Provider'}
+                          hidden={user != 'Provider'}
+                          className="button is-success  px-6 py-3 font-bold text-gray-100 hocus:bg-primary-700 hocus:text-gray-200 focus:shadow-outline focus:outline-none transition duration-300 text-sm rounded-full m-5"
+                        >
+                          Mark Completed
+                        </CardButton2>
+                      </>
+                    )}
+                  </Column>
+                </TwoColumn>
+              </Card>
+            </CardContainer>
+          ))}
+        </SlideshowWithPagination>
+      ) : (
+        <Message color={'danger'}>
+          <Message.Body>No Requests! Check Later</Message.Body>
+        </Message>
+      )}
+    </>
+  );
+};
+
+export default Requests;
--- pages/view_serviceRequest_status_provider.js
+++ pages/view_serviceRequest_status_provider.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Layout from '../components/Layout';
 import { motion } from 'framer-motion';
 const _ = require('underscore');
-import Header from "../components/Header"
+import Header from '../components/Header';
 import {
   PrimaryButton,
   PrimaryButton2,
@@ -21,11 +21,15 @@ import { css } from 'styled-components/macro'; //eslint-disable-line
 import { Container, ContentWithPaddingXl } from '../components/misc/Layouts.js';
 import { SectionHeading } from '../components/misc/Headings.js';
 
-
 //import { Loader } from "react-feather";
-import { GET_ME_AS_SERVICE_PROVIDER,  GET_SERVICE_REQUESTS_FOR_ME } from "../gql/query";
+import {
+  GET_ME_AS_SERVICE_PROVIDER,
+  GET_SERVICE_REQUESTS_FOR_ME
+} from '../gql/query';
 import { REJECT_SR } from '../gql/mutation';
 
+import SlideshowWithPagination from 'react-slideshow-with-pagination';
+import Requests from '../components/Requests';
 
 const HeaderRow = tw.div`flex justify-between items-center flex-col xl:flex-row`;
 const TwoColumn = tw.div`flex flex-col sm:flex-row justify-between`;
@@ -77,12 +81,8 @@ const CardHoverOverlay = styled(motion.div)`
   background-color: rgba(255, 255, 255, 0.5);
   ${tw`absolute inset-0 flex justify-center items-center`}
 `;
-const CardButton = tw(
-  PrimaryButtonBase
-)`text-sm rounded-full m-5 bg-green-500`;
-const CardButton2 = tw(
-  PrimaryButtonBase
-)`text-sm rounded-full m-5 bg-blue-600`;
+const CardButton = tw(PrimaryButtonBase)`text-sm rounded-full m-5 bg-green-500`;
+const CardButton2 = tw(PrimaryButtonBase)`text-sm rounded-full m-5 bg-blue-600`;
 const CardButtonUnAvailable = tw(
   DisabledButtonBase
 )`text-sm rounded-full m-5 bg-red-500`;
@@ -93,250 +93,110 @@ const CardText = tw.div`p-4 text-gray-900`;
 const CardTitle = tw.h5`text-lg font-semibold group-hover:text-primary-500`;
 const CardContent = tw.p`mt-1 text-sm font-medium text-gray-600`;
 
-const ServiceProviderStatusPage=({history})=>{
-    const {loading,error,data} = useQuery(GET_ME_AS_SERVICE_PROVIDER);
+const ServiceProviderStatusPage = ({ history }) => {
+  const { loading, error, data } = useQuery(GET_ME_AS_SERVICE_PROVIDER);
 
-    const requestsForMe=useQuery(GET_SERVICE_REQUESTS_FOR_ME);
+  const requestsForMe = useQuery(GET_SERVICE_REQUESTS_FOR_ME);
 
-    const { addToast } = useToasts();
-    //const[values,setValues]=useState();
-    
+  const { addToast } = useToasts();
+  //const[values,setValues]=useState();
 
-    const [rejectServiceRequest,{loading_cancel,error_cancel}] = useMutation(REJECT_SR,{
-        onCompleted:(data)=>{
-          addToast("Successfully rejected request",{appearance:"success"})
-          history.push(`/profile/serviceRequestsForMe`)
-        },
-        onError:(error)=>{
-          addToast("Failed ",{appearance:"error"})
-        }
-        
-        
-      });
+  const [rejectServiceRequest, { loading_cancel, error_cancel }] = useMutation(
+    REJECT_SR,
+    {
+      onCompleted: data => {
+        addToast('Successfully rejected request', { appearance: 'success' });
+        history.push(`/profile/serviceRequestsForMe`);
+      },
+      onError: error => {
+        addToast('Failed ', { appearance: 'error' });
+      }
+    }
+  );
 
-    if(loading|| requestsForMe.loading) return <Loader/>
+  if (loading || requestsForMe.loading) return <Loader />;
 
+  const acceptedRequests = requestsForMe.data.acceptedServiceRequestsForMe;
+  const pendingRequests = requestsForMe.data.pendingServiceRequestsForMe;
+  const startedRequests = requestsForMe.data.startedServiceRequestsForMe;
+  const completedRequests = requestsForMe.data.completedServiceRequestsForMe;
+  const canceledRequests = requestsForMe.data.canceledServiceRequestsForMe;
+  const rejectedRequests = requestsForMe.data.rejectedServiceRequestsForMe;
+  if (error) return <Redirect to={'/'} />;
+  return (
+    <Container>
+      <Header />
+      <ContentWithPaddingXl>
+        <HeaderRow>
+          <Heading>Started Requests</Heading>
+        </HeaderRow>
+        <br/>
+        <Requests
+          requests={startedRequests}
+          loading={loading}
+          state="Started"
+          user="Provider"
+        />
 
-    const acceptedRequests=requestsForMe.data.acceptedServiceRequestsForMe;
-    const pendingRequests=requestsForMe.data.pendingServiceRequestsForMe;
-    const startedRequests=requestsForMe.data.startedServiceRequestsForMe;
-    const completedRequests=requestsForMe.data.completedServiceRequestsForMe;
-    const canceledRequests=requestsForMe.data.canceledServiceRequestsForMe;
-    const rejectedRequests=requestsForMe.data.rejectedServiceRequestsForMe;
-    if(error) return <Redirect to={"/"}/>
-    return (
-        <Container>
-          <Header/>
-          <ContentWithPaddingXl>
-          <HeaderRow>
-              <Heading>Started Requests</Heading>
-            </HeaderRow>
-            
-            {startedRequests
-              .map((request, index) => (
-                <CardContainer key={index}>
-                  <Card
-                    className="group"
-                    href={request.url}
-                    initial="rest"
-                    whileHover="hover"
-                    animate="rest"
-                  >
-                    <TwoColumn>
-                      
-                      <Column>
-                        <CardText>
-                          <CardTitle>{request.task}</CardTitle>
-                          <CardContent>{request.date} at {request.time}Hrs</CardContent>
-                        </CardText>
-                      </Column>
-                     
-                      <Column>
-                        
-                        <CardButton2 className="button is-danger">Mark Completed</CardButton2>
-                       
-                      </Column>
-                    </TwoColumn>
-                  </Card>
-                </CardContainer>
-              ))}
-              <HeaderRow>
-              <Heading>Canceled Requests</Heading>
-            </HeaderRow>
-            
-            {canceledRequests
-              .map((request, index) => (
-                <CardContainer key={index}>
-                  <Card
-                    className="group"
-                    href={request.url}
-                    initial="rest"
-                    whileHover="hover"
-                    animate="rest"
-                  >
-                    <TwoColumn>
-                      
-                      <Column>
-                        <CardText>
-                          <CardTitle>{request.task}</CardTitle>
-                          <CardContent>{request.date} at {request.time}Hrs</CardContent>
-                        </CardText>
-                      </Column>
-                     
-                      <Column>
-                        
-                        <CardButton2 className="button is-danger">Mark Completed</CardButton2>
-                       
-                      </Column>
-                    </TwoColumn>
-                  </Card>
-                </CardContainer>
-              ))}
-            <HeaderRow>
-              <Heading>Accepted Requests</Heading>
-            </HeaderRow>
-            
-            {acceptedRequests
-              .map((request, index) => (
-                <CardContainer key={index}>
-                  <Card
-                    className="group"
-                    href={request.url}
-                    initial="rest"
-                    whileHover="hover"
-                    animate="rest"
-                  >
-                    <TwoColumn>
-                      
-                      <Column>
-                        <CardText>
-                          <CardTitle>{request.task}</CardTitle>
-                          <CardContent>{request.date} at {request.time}Hrs</CardContent>
-                        </CardText>
-                      </Column>
-                     
-                      <Column>
-                        <CardButton2 className="button is-info">View</CardButton2>
-                        <CardButton2 className="button is-danger"  onClick={event=>{
-                            rejectServiceRequest({
-                                variables:{
-                                    rejectServiceRequestId:request.id
-                                }})
-                        }}>Reject</CardButton2>
-                       
-                      </Column>
-                    </TwoColumn>
-                  </Card>
-                </CardContainer>
-              ))}
+        <HeaderRow>
+          <Heading>Canceled Requests</Heading>
+        </HeaderRow>
+        <br/>
+        <Requests
+          requests={canceledRequests}
+          loading={loading}
+          state="Canceled"
+          user="Provider"
+        />
 
-<HeaderRow>
-              <Heading>Pending Requests</Heading>
-            </HeaderRow>
-            
-            {pendingRequests
-              .map((request, index) => (
-                <CardContainer key={index}>
-                  <Card
-                    className="group"
-                    href={request.url}
-                    initial="rest"
-                    whileHover="hover"
-                    animate="rest"
-                  >
-                    <TwoColumn>
-                      
-                      <Column>
-                        <CardText>
-                          <CardTitle>{request.task}</CardTitle>
-                          <CardContent>{request.date} at {request.time}Hrs</CardContent>
-                        </CardText>
-                      </Column>
-                     
-                      <Column>
-                        <CardButton2 className="button is-info">View</CardButton2>
-                        <CardButton2 className="button is-info">Accept</CardButton2>
-                        <CardButton2 className="button is-danger" onClick={event=>{
-                            rejectServiceRequest({
-                                variables:{
-                                    rejectServiceRequestId:request.id
-                                }})
-                        }}>Reject</CardButton2>
-                      
-                       
-                      </Column>
-                    </TwoColumn>
-                  </Card>
-                </CardContainer>
-              ))}
-              <HeaderRow>
-              <Heading>Completed Requests</Heading>
-            </HeaderRow>
-            
-            {completedRequests
-              .map((request, index) => (
-                <CardContainer key={index}>
-                  <Card
-                    className="group"
-                    href={request.url}
-                    initial="rest"
-                    whileHover="hover"
-                    animate="rest"
-                  >
-                    <TwoColumn>
-                      
-                      <Column>
-                        <CardText>
-                          <CardTitle>{request.task}</CardTitle>
-                          <CardContent>{request.date} at {request.time}Hrs</CardContent>
-                        </CardText>
-                      </Column>
-                     
-                      <Column>
-                        <CardButton2 className="button is-info">View</CardButton2>
-                        
-                       
-                      </Column>
-                    </TwoColumn>
-                  </Card>
-                </CardContainer>
-              ))}
-               <HeaderRow>
-              <Heading>Rejected Requests</Heading>
-            </HeaderRow>
-            
-            {rejectedRequests
-              .map((request, index) => (
-                <CardContainer key={index}>
-                  <Card
-                    className="group"
-                    href={request.url}
-                    initial="rest"
-                    whileHover="hover"
-                    animate="rest"
-                  >
-                    <TwoColumn>
-                      
-                      <Column>
-                        <CardText>
-                          <CardTitle>{request.task}</CardTitle>
-                          <CardContent>{request.date} at {request.time}Hrs</CardContent>
-                        </CardText>
-                      </Column>
-                     
-                      <Column>
-                        
-                        <CardButton2 className="button is-danger">Mark Completed</CardButton2>
-                       
-                      </Column>
-                    </TwoColumn>
-                  </Card>
-                </CardContainer>
-              ))}
-          </ContentWithPaddingXl>
-        </Container>
-      );
-    };
-    
-    export default ServiceProviderStatusPage;
-    
+        <HeaderRow>
+          <Heading>Accepted Requests</Heading>
+        </HeaderRow>
+        <br/>
+
+        <Requests
+          requests={acceptedRequests}
+          loading={loading}
+          state="Accepted"
+          user="Provider"
+        />
+
+        <HeaderRow>
+          <Heading>Pending Requests</Heading>
+        </HeaderRow>
+        <br/>
+        <Requests
+          requests={pendingRequests}
+          loading={loading}
+          state="Pending"
+          user="Provider"
+        />
+
+        <HeaderRow>
+          <Heading>Completed Requests</Heading>
+        </HeaderRow>
+        <br/>
+
+        <Requests
+          requests={completedRequests}
+          loading={loading}
+          state="Completed"
+          user="Provider"
+        />
+
+        <HeaderRow>
+          <Heading>Rejected Requests</Heading>
+        </HeaderRow>
+        <br/>
+        <Requests
+          requests={rejectedRequests}
+          loading={loading}
+          state="Rejected"
+          user="Provider"
+        />
+      </ContentWithPaddingXl>
+    </Container>
+  );
+};
+
+export default ServiceProviderStatusPage;
